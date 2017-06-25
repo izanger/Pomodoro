177d972ecb95fd5d6fe95e71a7c48e512f3a8924
Ian Zanger <izanger@purdue.edu>
2017-06-25 00:39:39 -0400

    Add cyclePomodoro function and introductory greeting

--- src/Pomodoro.java
+++ src/Pomodoro.java
@@ -1,29 +1,45 @@
 /**
  * Created by ianzanger on 6/24/17.
  */
+import java.util.Scanner;
 public class Pomodoro {
+    public static int defaultPomLength = 25;
+    public static int defaultShortBreakLength = 5;
+    public static int defaultLongBreakLength = 15;
     public static void main(String[] args) {
-        try{
+        Scanner s = new Scanner(System.in);
+        System.out.println("Press <Enter> for a standard Pomodoro cycle, or any key + <Enter> for a custom cycle.");
+        String input = s.nextLine();
 
-            for(int i = 0; i < 4; i++) {
-                if(i > 0) {
-                    System.out.print("5 minutes elapsed. ");
-                }
-
-                System.out.println("Starting timer for 25 minutes.");
-                Thread.sleep(5 * 1000); //replace with 25min when done testing: 25 * 60 * 1000
-
-                if(i == 3){
-                    System.out.println("25 minutes elapsed. Starting timer for 15 minute break.");
-                    Thread.sleep(10 * 1000); //replace with 15 * 60 * 1000
-                } else {
-                    System.out.println("25 minutes elapsed. Starting timer for 5 minute break.");
-                    Thread.sleep(1000 * 5); //replace with 5 * 60 * 1000
-                }
+        if(input.length() == 0) {
+            try {
+                cyclePomodoro(defaultPomLength, defaultShortBreakLength, defaultLongBreakLength);
+            } catch (InterruptedException e) {
+                System.err.println("Interrupted exception");
             }
-
-        } catch (InterruptedException e) {
-            System.err.println("Interrupted exception");
         }
     }
+
+    public static void cyclePomodoro(int pomLength, int shortBreakLength, int longBreakLength) throws InterruptedException {
+
+        for(int i = 0; i < 4; i++) {
+            if(i > 0) {
+                System.out.print("5 minutes elapsed. ");
+            }
+
+            System.out.println("Starting timer for 25 minutes.");
+            Thread.sleep(5 * 1000); //replace with 25min when done testing: 25 * 60 * 1000
+
+            if(i == 3){
+                System.out.println("25 minutes elapsed. Starting timer for 15 minute break.");
+                Thread.sleep(10 * 1000); //replace with 15 * 60 * 1000
+            } else {
+                System.out.println("25 minutes elapsed. Starting timer for 5 minute break.");
+                Thread.sleep(1000 * 5); //replace with 5 * 60 * 1000
+            }
+        }
+
+        //Scanner s = new Scanner(System.in);
+    }
+
 }
